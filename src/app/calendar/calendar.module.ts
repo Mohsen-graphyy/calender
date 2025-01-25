c6c2841633c0c9c5f4b3289639ff901a0ae8d843
mohsen piri <hajmohsenpiri@gmail.com>
2025-01-26 01:53:25 +0330

    add calendar module with routing and initial component

---
+++ src/app/calendar/calendar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarComponent } from './calendar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: CalendarComponent }];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes), CalendarComponent],
+})
+export class CalendarModule {}
