250721d5d4f9bc01b9e41755f739406244ac406c
mohsen piri <hajmohsenpiri@gmail.com>
2025-01-26 13:20:27 +0330

    implement calendar component layout and functionality with header controls

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'calendar',
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
+  imports: [MatIconModule, MatButtonModule],
 })
-export class CalendarComponent {}
+export class CalendarComponent {
+  viewDate: Date = new Date();
+  currentView = 'month';
+}
