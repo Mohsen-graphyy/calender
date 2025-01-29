e4c41c279dc9365a02d3a778304c27608898c4a9
mohsen piri <hajmohsenpiri@gmail.com>
2025-01-29 03:43:25 +0330

    feat(about-me): enhance AboutMeComponent with detailed profile and styling

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-about-me',
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, MatIconModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  stars = Array(5).fill(0);
+}
